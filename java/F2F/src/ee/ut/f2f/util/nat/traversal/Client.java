469f87756144e77ab5fd6618775fd9910e1bf4e0
timofei77 <timofei77@e90d3511-7839-0410-915a-49ea6329ddb5>
2007-11-18 13:10:40 +0000

    NAT Traversal messaging modified client and server classes

--- java/F2F/src/ee/ut/f2f/util/nat/traversal/Client.java
+++ java/F2F/src/ee/ut/f2f/util/nat/traversal/Client.java
@@ -1,20 +1,19 @@
 package ee.ut.f2f.util.nat.traversal;
 
-import java.io.BufferedReader;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-public class Client {
+public class Client extends Thread {
 	
 	private String ip = null;
 	private int port = -1;
 	
 	private Socket soc = null;
-    private PrintWriter out = null;
-    private BufferedReader in = null;
+    private BufferedOutputStream out = null;
+    private BufferedInputStream in = null;
     
     private final static NatLogger log = new NatLogger(Client.class);
 	
@@ -24,6 +23,7 @@ public class Client {
 	 * @param port
 	 */
 	public Client(String ip, int port){
+		super("F2F Client");
 		if (ip == null || port <=0 ) throw new IllegalArgumentException();
 		this.ip = ip;
 		this.port = port;
@@ -32,13 +32,34 @@ public class Client {
 	/**
 	 * Connect to ip and port specified in constructor
  	 */
-	public void connect(){
+	@Override
+	public void run(){
 		try {
 			soc = new Socket(ip, port);
             log.debug("Connecting to server [" + ip + ":" + port + "]" );
-            //TODO ByteArrayInputStream and ByteArrayOutputStream should be used
-            out = new PrintWriter(soc.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+            out = new BufferedOutputStream(soc.getOutputStream());
+            in = new BufferedInputStream(soc.getInputStream());
+            
+            while(true){
+            	byte[] bytes = new byte[in.available()];
+            	in.read(bytes);
+            	String received = new String(bytes,"utf-8");
+            
+            	if(received != null && !"".equals(received)) log.debug(">>> [" + received + "]");
+            	if("HELO".equals(received)){
+                    bytes = "BYE".getBytes("utf-8");
+                    log.debug("<<< [BYE]");
+                    out.write(bytes);
+                    out.flush();
+            	} else if ("BYE".equals(received)){
+            		break;
+            	}
+            }
+            log.debug("Closing client");
+            in.close();
+            out.close();
+            soc.close();
+            
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- java/F2F/src/ee/ut/f2f/util/nat/traversal/Server.java
+++ java/F2F/src/ee/ut/f2f/util/nat/traversal/Server.java
@@ -1,5 +1,7 @@
 package ee.ut.f2f.util.nat.traversal;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -8,6 +10,8 @@ import java.util.Map;
 
 public class Server extends Thread {
 
+	private final static NatLogger log = new NatLogger(Client.class);
+	
 	ServerSocket serverSocket = null;
 	Map<String, Socket> clients = new Hashtable<String, Socket>();
 	
@@ -28,9 +32,35 @@ public class Server extends Thread {
 	@Override
 	public void run() {
 		Socket clientSocket = null;
+		BufferedInputStream bis = null;
+		BufferedOutputStream bos = null;
 		while( true ){
 			try {
 				clientSocket = serverSocket.accept();
+				log.debug("New client connected from [" + clientSocket.getInetAddress().getHostAddress() + ":" + clientSocket.getPort() + "]");
+				bis = new BufferedInputStream( clientSocket.getInputStream() );
+				bos = new BufferedOutputStream( clientSocket.getOutputStream() );
+				
+				log.debug("<<< [HELO]");
+				bos.write("HELO".getBytes("utf-8"));
+				bos.flush();
+				while(true){
+					byte[] bytes = new byte[bis.available()];
+					bis.read(bytes);
+					String received = new String(bytes,"utf-8");
+					if(received != null && !"".equals(received)) log.debug(">>> [" + received + "]");
+					if("BYE".equals(received)){
+						log.debug("<<< [BYE]");
+						bos.write(bytes);
+						bos.flush();
+						break;
+					}
+				}
+				log.debug("Closing client socket");
+				bis.close();
+				bos.close();
+				clientSocket.close();
+				log.debug("Server continues listening ...");
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -40,7 +70,7 @@ public class Server extends Thread {
 	
 	/**
 	 * Send data to client
-	 * @param id Client's Id (example temp_7777@msn.com)
+	 * @param id Client's Id
 	 * @param data
 	 */
 	public void send(String id, byte[] data){
