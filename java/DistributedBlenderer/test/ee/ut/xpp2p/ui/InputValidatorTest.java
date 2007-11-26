87443c4584bb8a313c2da85693a6d13064571e8c
voviss <voviss@e90d3511-7839-0410-915a-49ea6329ddb5>
2007-11-26 11:22:49 +0000

    refactored

--- java/DistributedBlenderer/test/ee/ut/xpp2p/ui/InputValidatorTest.java
+++ java/DistributedBlenderer/test/ee/ut/xpp2p/ui/InputValidatorTest.java
@@ -11,124 +11,126 @@ import ee.ut.xpp2p.blenderer.MasterBlenderer;
  * @created 03.11.2007
  */
 public class InputValidatorTest extends TestCase {
-	
+
 	static MainWindow window = null;
 
 	/**
-	 * Test method for {@link ee.ut.xpp2p.ui.InputValidator#validate()}.
-	 * Tested through the user interface MainWindow
+	 * Test method for {@link ee.ut.xpp2p.ui.InputValidator#validate()}. Tested
+	 * through the user interface MainWindow
 	 */
-	public void testRenderButtonPressed() {
-		// testing valid input parameters
-		
-		// test selecting a valid input file
-		setUp();
+
+	// testing valid input parameters
+	public void testSelectingValidInputFile() {
 		window.inputFileTextField.setText("etc\\VictorDancing.blend");
-		window.renderButton.getActionListeners()[0].actionPerformed(new ActionEvent(window.renderButton, 2, ""));
+		window.renderButton.getActionListeners()[0]
+				.actionPerformed(new ActionEvent(window.renderButton, 2, ""));
 		assertEquals(window.inputFileErrorLabel.getText(), "");
-		assertEquals(window.inputFileTextField.getText(), "etc\\VictorDancing.blend");
-		tearDown();
-		
-		// test selecting a valid output location
-		setUp();
+		assertEquals(window.inputFileTextField.getText(),
+				"etc\\VictorDancing.blend");
+	}
+	
+	public void testSelectingValidOutputLocation(){
 		window.outputLocTextField.setText("etc/");
-		window.renderButton.getActionListeners()[0].actionPerformed(new ActionEvent(window.renderButton, 2, ""));
+		window.renderButton.getActionListeners()[0]
+				.actionPerformed(new ActionEvent(window.renderButton, 2, ""));
 		assertEquals(window.outputLocErrorLabel.getText(), "");
 		assertEquals(window.outputLocTextField.getText(), "etc/");
-		tearDown();
-
-		// test startFrame number equals endFrame number
-		setUp();
+	}
+	
+	public void testStartFrameEqualsEndFrame(){
 		window.startFrameTextField.setText("5");
 		window.endFrameTextField.setText("5");
-		window.renderButton.getActionListeners()[0].actionPerformed(new ActionEvent(window.renderButton, 2, ""));
+		window.renderButton.getActionListeners()[0]
+				.actionPerformed(new ActionEvent(window.renderButton, 2, ""));
 		assertEquals(window.framesErrorLabel.getText(), "");
 		assertEquals(window.startFrameTextField.getText(), "5");
 		assertEquals(window.endFrameTextField.getText(), "5");
-		tearDown();
-		
-		// test startFrame number less than endFrame number
-		setUp();
+	}
+	
+	public void testStartFrameLessThanEndFrame(){
 		window.startFrameTextField.setText("3");
 		window.endFrameTextField.setText("5");
-		window.renderButton.getActionListeners()[0].actionPerformed(new ActionEvent(window.renderButton, 2, ""));
+		window.renderButton.getActionListeners()[0]
+				.actionPerformed(new ActionEvent(window.renderButton, 2, ""));
 		assertEquals(window.framesErrorLabel.getText(), "");
 		assertEquals(window.startFrameTextField.getText(), "3");
 		assertEquals(window.endFrameTextField.getText(), "5");
-		tearDown();
-		
-		// testing empty input parameters
-		
-		// test no input file, output location or startFrame specified
-		setUp();
+	}
+	
+	public void testNoInputParametersSpecified(){
 		window.startFrameTextField.setText("");
-		window.renderButton.getActionListeners()[0].actionPerformed(new ActionEvent(window.renderButton, 2, ""));
-		assertEquals(window.inputFileErrorLabel.getText(), "Input file must be specified");
-		assertEquals(window.outputLocErrorLabel.getText(), "Output location must be specified");
-		assertEquals(window.framesErrorLabel.getText(), "Start frame must be specified");
-		tearDown();
-		
-		// test no endFrame specified
-		setUp();
+		window.renderButton.getActionListeners()[0]
+				.actionPerformed(new ActionEvent(window.renderButton, 2, ""));
+		assertEquals(window.inputFileErrorLabel.getText(),
+				"Input file must be specified");
+		assertEquals(window.outputLocErrorLabel.getText(),
+				"Output location must be specified");
+		assertEquals(window.framesErrorLabel.getText(),
+				"Start frame must be specified");
+	}
+	
+	public void testNoEndFrameSpecified(){
 		window.endFrameTextField.setText("");
-		window.renderButton.getActionListeners()[0].actionPerformed(new ActionEvent(window.renderButton, 2, ""));
-		assertEquals(window.framesErrorLabel.getText(), "End frame must be specified");
-		tearDown();
-		
-		//test  invalid input parameters
-		
-		// test nonexistent input file
-		setUp();
+		window.renderButton.getActionListeners()[0]
+				.actionPerformed(new ActionEvent(window.renderButton, 2, ""));
+		assertEquals(window.framesErrorLabel.getText(),	"End frame must be specified");
+	}
+	
+	public void testNonexistentInputFile(){
 		window.inputFileTextField.setText("etc\\nonexistent.blend");
 		window.renderButton.getActionListeners()[0].actionPerformed(new ActionEvent(window.renderButton, 2, ""));
 		assertEquals(window.inputFileErrorLabel.getText(), "Input file doesn't exist");
-		tearDown();
-		
-		// test nonexistent output folder
-		setUp();
+	}
+	
+	public void testNonexistentOutputFolder(){
 		window.outputLocTextField.setText("nonexistent/");
-		window.renderButton.getActionListeners()[0].actionPerformed(new ActionEvent(window.renderButton, 2, ""));
-		assertEquals(window.outputLocErrorLabel.getText(), "Output location doesn't exist");
-		tearDown();
-		
-		// test no write permission on output folder
-		setUp();
+		window.renderButton.getActionListeners()[0]
+				.actionPerformed(new ActionEvent(window.renderButton, 2, ""));
+		assertEquals(window.outputLocErrorLabel.getText(),
+				"Output location doesn't exist");
+	}
+	
+	public void testNoWritePermissionOnOutputFolder(){
 		window.outputLocTextField.setText("etc/readOnlyLocation/");
 		new File("etc/readOnlyLocation").setReadOnly();
-		window.renderButton.getActionListeners()[0].actionPerformed(new ActionEvent(window.renderButton, 2, ""));
-		assertEquals(window.outputLocErrorLabel.getText(), "No write permission on the specified output location");
-		tearDown();
-		
-		// non-numeric startframe
-		setUp();
+		window.renderButton.getActionListeners()[0]
+				.actionPerformed(new ActionEvent(window.renderButton, 2, ""));
+		assertEquals(window.outputLocErrorLabel.getText(),
+				"No write permission on the specified output location");
+	}
+	
+	public void testNonnumericStartFrame(){
 		window.startFrameTextField.setText("text");
-		window.renderButton.getActionListeners()[0].actionPerformed(new ActionEvent(window.renderButton, 2, ""));
-		assertEquals(window.framesErrorLabel.getText(), "Frames must be specified as integers");
-		tearDown();
-		
-		// non-numeric endframe
-		setUp();
+		window.renderButton.getActionListeners()[0]
+				.actionPerformed(new ActionEvent(window.renderButton, 2, ""));
+		assertEquals(window.framesErrorLabel.getText(),
+				"Frames must be specified as integers");
+	}
+	
+	public void testNonnumericEndFrame(){
 		window.endFrameTextField.setText("text");
-		window.renderButton.getActionListeners()[0].actionPerformed(new ActionEvent(window.renderButton, 2, ""));
-		assertEquals(window.framesErrorLabel.getText(), "Frames must be specified as integers");
-		tearDown();
-		
-		// test startFrame number bigger than endFrame number
-		setUp();
+		window.renderButton.getActionListeners()[0]
+				.actionPerformed(new ActionEvent(window.renderButton, 2, ""));
+		assertEquals(window.framesErrorLabel.getText(),
+				"Frames must be specified as integers");
+	}
+
+	public void testStartFrameBiggerThanEndFrame() {
 		window.startFrameTextField.setText("5");
 		window.endFrameTextField.setText("3");
-		window.renderButton.getActionListeners()[0].actionPerformed(new ActionEvent(window.renderButton, 2, ""));
-		assertEquals(window.framesErrorLabel.getText(), "Starting frame number can't be bigger than ending frame number");
+		window.renderButton.getActionListeners()[0]
+				.actionPerformed(new ActionEvent(window.renderButton, 2, ""));
+		assertEquals(window.framesErrorLabel.getText(),
+				"Starting frame number can't be bigger than ending frame number");
 		assertEquals(window.startFrameTextField.getText(), "5");
 		assertEquals(window.endFrameTextField.getText(), "3");
-		tearDown();
 	}
-	
-	protected void setUp(){
+
+	protected void setUp() {
 		window = new MainWindow(new MasterBlenderer());
 	}
-	
-	protected void tearDown(){
+
+	protected void tearDown() {
 		window.dispose();
 		window.inputFileTextField.setText("");
 		window.outputLocTextField.setText("");
@@ -138,5 +140,5 @@ public class InputValidatorTest extends TestCase {
 		window.outputLocErrorLabel.setText("");
 		window.framesErrorLabel.setText("");
 	}
-	
+
 }
