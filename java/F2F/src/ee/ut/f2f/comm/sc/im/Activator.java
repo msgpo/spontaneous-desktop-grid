b3bd1e6087229eff0610e09f64ce26ede9241b88
K.Kraaner <K.Kraaner@e90d3511-7839-0410-915a-49ea6329ddb5>
2008-02-04 11:09:13 +0000

    * the F2F framework is not started if configuration file doesn't allow this;

--- java/F2F/src/ee/ut/f2f/comm/sc/im/Activator.java
+++ java/F2F/src/ee/ut/f2f/comm/sc/im/Activator.java
@@ -5,6 +5,7 @@ import org.osgi.framework.BundleContext;
 
 import ee.ut.f2f.core.F2FComputing;
 import ee.ut.f2f.ui.F2FComputingGUI;
+import ee.ut.f2f.util.F2FProperties;
 
 public class Activator implements BundleActivator {
 
@@ -14,6 +15,9 @@ public class Activator implements BundleActivator {
 	 */
 	public void start(BundleContext context) throws Exception
 	{
+		if (!F2FProperties.getF2FProperties().getCommLayerProperties().bInitSip)
+			return;
+		
 		// init GUI
 		F2FComputingGUI.main(new String[]{});
 		// init F2F framework
@@ -26,8 +30,9 @@ public class Activator implements BundleActivator {
 	 * (non-Javadoc)
 	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
-	public void stop(BundleContext context) throws Exception {
-		System.out.println("f2f-plugin: Stop F2F SC-plugin");
+	public void stop(BundleContext context) throws Exception
+	{
+		//System.out.println("f2f-plugin: Stop F2F SC-plugin");
 	}
 
 }
