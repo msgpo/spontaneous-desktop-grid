8040f09893dcd9e2087883b12d5ebe27f153e9c3
K.Kraaner <K.Kraaner@e90d3511-7839-0410-915a-49ea6329ddb5>
2008-02-15 16:27:06 +0000

    * support for slave tasks to be prepared with the initial data in the master peer;

---
+++ java/F2F-Compute-Pi/src/ee/ut/f2f/examples/pi/PiMaster2.java
@@ -0,0 +1,127 @@
+package ee.ut.f2f.examples.pi;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import ee.ut.f2f.core.CommunicationFailedException;
+import ee.ut.f2f.core.F2FComputing;
+import ee.ut.f2f.core.F2FPeer;
+import ee.ut.f2f.core.Task;
+import ee.ut.f2f.core.TaskProxy;
+import ee.ut.f2f.util.F2FDebug;
+
+public class PiMaster2 extends Task
+{
+	// number of milliseconds to the slaves, how long
+	// they should compute points
+	Long intervalms = 2000L;
+	// received points
+	AtomicLongVector received = new AtomicLongVector( 0, 0 );
+	// How many points to compute in total
+	long maxpoints = 1000000000L;
+	
+	public void runTask()
+	{
+		long start = System.currentTimeMillis();
+		
+		// prepare the slave tasks
+		Collection<Task> slaveTasks = new ArrayList<Task>();
+		for (int i = 0; i < this.getJob().getPeers().size(); i++)
+		{
+			PiSlave2 task = new PiSlave2();
+			task.setInterval(2000);
+			slaveTasks.add(task);
+		}
+		
+		// submit slave tasks
+		try {
+			// HACK for running PiTest
+			// start 2 slaves in localhost if job is created without specifying friends
+			if (this.getJob().getPeers() == null || this.getJob().getPeers().size() == 0)
+			{
+				Collection<F2FPeer> peer = new ArrayList<F2FPeer>();
+				peer.add(F2FComputing.getLocalPeer());
+				Collection<Task> task = new ArrayList<Task>();
+				task.add(slaveTasks.iterator().next());
+				for (int i = 0; i < 2; i++)
+					this.getJob().submitTasks(
+							task,
+							peer);
+			}
+			else
+			{
+				this.getJob().submitTasks(
+					slaveTasks,
+					this.getJob().getPeers());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return;
+		}
+				
+		// show current result after each 10 seconds
+		// the loop is exited after enough results have been received from slaves
+		while (received.getUnSyncTotal() < maxpoints)
+		{
+			F2FDebug.println("processed " + (int)(((float)received.getUnSyncTotal() / maxpoints)*100) + "%" );
+			F2FDebug.println("Pi is " + received.getUnSyncPositive() * 4.0 / received.getUnSyncTotal() );
+			try {
+				Thread.sleep(10000);
+			} catch (InterruptedException e) {}
+		}
+		
+		// send stop message to slave tasks
+		for (String taskID: this.getJob().getTaskIDs())
+		{
+			if (taskID == this.getTaskID()) continue;
+			TaskProxy proxy = this.getTaskProxy(taskID);
+			if (proxy == null) continue;
+			try {
+				proxy.sendMessage(new Long(0L));
+			} catch (CommunicationFailedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		long end = System.currentTimeMillis();
+
+		// show the result
+		F2FDebug.println("The computed Pi is : " + received.getUnSyncPositive() * 4.0 / received.getUnSyncTotal());
+		F2FDebug.println("Took " + (end - start) / 1000 + "s");
+		System.out.println("The computed Pi is : " + 
+				received.getUnSyncPositive() * 4.0 / received.getUnSyncTotal());
+		System.out.println("Took " + (end - start) / 1000 + "s");
+	}
+	
+	// collect results
+	public void messageReceived(String remoteTaskID)
+	{
+		// do not process the message if the required amount of 
+		// points have been calculated already
+		if (received.getUnSyncTotal() >= maxpoints) return;
+		
+		// process the numbers from the slave task
+		TaskProxy proxy = this.getTaskProxy(remoteTaskID);
+		if (!proxy.hasMessage()) return;
+		AtomicLongVector receivedvector = (AtomicLongVector) proxy.receiveMessage();
+		synchronized (this)
+		{
+			if (received.getUnSyncTotal() >= maxpoints) return;
+			received.add( receivedvector );
+			
+			F2FDebug.println("Received from task "+remoteTaskID+": total " 
+					+ receivedvector.getUnSyncTotal()
+					+ " positives " 
+					+ receivedvector.getUnSyncPositive());
+			F2FDebug.println("Sum is now: total " 
+					+ received.getUnSyncTotal()
+					+ " positives " 
+					+ received.getUnSyncPositive());
+			
+			// check if the job is finished
+			if (received.getUnSyncTotal() >= maxpoints)
+				this.interrupt();
+		}
+	}
+}
---
+++ java/F2F-Compute-Pi/src/ee/ut/f2f/examples/pi/PiSlave2.java
@@ -0,0 +1,94 @@
+package ee.ut.f2f.examples.pi;
+
+import java.io.Serializable;
+
+import ee.ut.f2f.core.CommunicationFailedException;
+import ee.ut.f2f.core.Task;
+import ee.ut.f2f.core.TaskProxy;
+import ee.ut.f2f.util.F2FDebug;
+
+import org.uncommons.maths.random.MersenneTwisterRNG;
+
+public class PiSlave2 extends Task implements Serializable
+{
+	private static final long serialVersionUID = 3877180695685895155L;
+
+	AtomicLongVector computedPoints = new AtomicLongVector (0,0);
+	
+	long intervalms = 10000;
+	void setInterval(long intervalms)
+	{
+		this.intervalms = intervalms;
+	}
+	
+	boolean bStop = false;
+		
+	public void runTask()
+	{
+		// get proxy of master task
+		TaskProxy masterProxy = this.getTaskProxy(this.getJob().getMasterTaskID());
+		if (masterProxy == null) throw new RuntimeException("Proxy of master task was not found!");
+		
+		// Start a thread which computes the points
+		ComputePoints compTask = new ComputePoints ();
+		compTask.start();
+		
+		// Send the computed points back to the master
+		while (true)
+		{
+			// Wait
+			try {
+				Thread.sleep(intervalms);
+			} catch (InterruptedException e) {}
+			
+			// Test if stop message has been sent
+			if (bStop) break;
+				
+			F2FDebug.println("Stop-condition not met, sending back results. total: "
+					+ computedPoints.getUnSyncTotal() + " positives: " 
+					+ computedPoints.getUnSyncPositive() );
+			// send results back to master
+			try {
+				masterProxy.sendMessage( computedPoints );
+				// reset computiation thread
+				computedPoints.set(0, 0);
+			} catch (CommunicationFailedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		// stop the thread
+		compTask.run = false;
+	}
+
+	// end the calculations
+	public void messageReceived(String remoteTaskID)
+	{
+		if (!remoteTaskID.equals(this.getJob().getMasterTaskID())) return;
+		TaskProxy proxy = this.getTaskProxy(remoteTaskID);
+		if (!proxy.hasMessage()) return;
+		Long stopcondition = (Long) proxy.receiveMessage();
+		if (stopcondition.intValue() == 0) bStop = true;
+		this.interrupt();
+	}
+	
+	private class ComputePoints extends Thread
+	{
+		boolean run = true; 
+
+		public void run()
+		{
+			MersenneTwisterRNG random = new MersenneTwisterRNG();
+			while(run)
+			{
+				double x = random.nextDouble();
+				double y = random.nextDouble();
+				// check if the point in the quarter-cicle with radius 1
+				if( x*x + y*y < 1.0 )
+					computedPoints.positiveHit();
+				else
+					computedPoints.negativeHit();
+			}
+		}
+	}
+}
