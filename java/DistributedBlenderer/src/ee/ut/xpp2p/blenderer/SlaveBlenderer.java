d6b99d3d3c1d61d817f1a9d5f3cb09ca8dfd0a57
K.Kraaner <K.Kraaner@e90d3511-7839-0410-915a-49ea6329ddb5>
2008-03-14 12:58:41 +0000

    * removed jmf (jmf from SIP Communicator is used);

--- java/DistributedBlenderer/src/ee/ut/xpp2p/blenderer/SlaveBlenderer.java
+++ java/DistributedBlenderer/src/ee/ut/xpp2p/blenderer/SlaveBlenderer.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Properties;
 
-import ee.ut.f2f.core.CommunicationFailedException;
 import ee.ut.f2f.core.Task;
 import ee.ut.f2f.core.TaskProxy;
 import ee.ut.xpp2p.exception.NothingRenderedException;
@@ -40,38 +39,22 @@ public class SlaveBlenderer extends Task {
 		if (masterProxy == null)
 			throw new RuntimeException("Proxy of master task was not found!");
 
-		boolean taskReceived = false;
-		while (!taskReceived) {
-			RenderTask receivedRenderTask = (RenderTask) masterProxy
-					.receiveMessage();
-			if (receivedRenderTask != null) {
-				taskReceived = true;
-				try {
-					renderTask(receivedRenderTask);
-					RenderResult result = new RenderResult();
-					result.setEndFrame(receivedRenderTask.getEndFrame());
-					result.setStartFrame(receivedRenderTask.getStartFrame());
-					String fileName = FileUtil.generateOutputFileName(receivedRenderTask.getStartFrame(), receivedRenderTask.getEndFrame(), receivedRenderTask.getExtension());
-					result.setFileName(fileName);			
-					String outputFile = tempDir + fileName;
-					result.setRenderedPart(FileUtil.loadFile(outputFile));
-					masterProxy.sendMessage(result);
-					//deletes rendered and sended part
-					FileUtil.deleteFiles(outputFile);
-					break;
-				} catch (NothingRenderedException e) {
-					System.out.println(e.getMessage());
-					// TODO: Handle exception
-				} catch (IOException e) {
-					System.out.println(e.getMessage());
-					// TODO: Handle exception
-				}
-				catch (CommunicationFailedException cfe) {
-					System.out.println(cfe.getMessage());
-					// TODO: Handle exception
-				}
-			}
-			System.out.println("Line 72");
+		RenderTask receivedRenderTask = (RenderTask) masterProxy
+				.receiveMessage();
+		try {
+			renderTask(receivedRenderTask);
+			RenderResult result = new RenderResult();
+			result.setEndFrame(receivedRenderTask.getEndFrame());
+			result.setStartFrame(receivedRenderTask.getStartFrame());
+			String fileName = FileUtil.generateOutputFileName(receivedRenderTask.getStartFrame(), receivedRenderTask.getEndFrame(), receivedRenderTask.getExtension());
+			result.setFileName(fileName);			
+			String outputFile = tempDir + fileName;
+			result.setRenderedPart(FileUtil.loadFile(outputFile));
+			masterProxy.sendMessage(result);
+			//deletes rendered and sended part
+			FileUtil.deleteFiles(outputFile);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 
@@ -83,63 +66,58 @@ public class SlaveBlenderer extends Task {
 	 * @param task
 	 *            task to render
 	 * @throws NothingRenderedException
+	 * @throws IOException 
 	 */
-	public void renderTask(RenderTask task) throws NothingRenderedException {
-		try {
-			File blenderFile = new File(task.getFileName());
-			String blenderFileName = blenderFile.getName();
-			if (tempDir == null)
+	public void renderTask(RenderTask task) throws NothingRenderedException, IOException
+	{
+		File blenderFile = new File(task.getFileName());
+		String blenderFileName = blenderFile.getName();
+		if (tempDir == null)
+		{
+			Properties props = System.getProperties();
+			tempDir = props.getProperty("java.io.tmpdir");
+			if (!tempDir.endsWith(File.separator))
 			{
-				Properties props = System.getProperties();
-				tempDir = props.getProperty("java.io.tmpdir");
-				if (!tempDir.endsWith(File.separator))
-				{
-					tempDir += File.separator;
-				}
+				tempDir += File.separator;
 			}
-			String fullBlenderFileName = tempDir + blenderFileName;
-			System.out.println("Filename = " + fullBlenderFileName);
-			File file = FileUtil.saveFile(task.getBlenderFile(), fullBlenderFileName);
-			System.out.println("Saved file: " + file);
-			String[] cmdarr = { "blender", "-b", fullBlenderFileName, "-o",
-					tempDir, "-F", task.getFileFormat(), "-s",
-					String.valueOf(task.getStartFrame()), "-e",
-					String.valueOf(task.getEndFrame()), "-a", "-x", "1" };
-			System.out.println("Arguments: ");
-			for(int i = 0; i < cmdarr.length; i++ ) System.out.println(cmdarr[i]);
-			Process proc = Runtime.getRuntime().exec(cmdarr);
-			BufferedReader br = new BufferedReader(new InputStreamReader(proc
-					.getInputStream()));
-
-			String line;
-			boolean anythingRendered = false;
-			System.out.println("Buffered reader: ");
-			while ((line = br.readLine()) != null && !bStopFlag) {
-				System.out.println(line);
-				if (line.startsWith("'blender' is not recognized")
-						|| line.indexOf("blender") != -1
-						&& line.indexOf("not found") != -1)
-					throw new NothingRenderedException("Blender not found!");
-
-				else if (line.startsWith("Append frame")
-						|| line.startsWith("added frame")
-						|| line.startsWith("Writing frame")) {
-					System.out.println("Rendered frame " + line.split(" +")[2]);
-					anythingRendered = true;
-				}
-
-				else if (line.startsWith("Blender quit") && anythingRendered) {
-					System.out.println("Rendering finished");
-				}
-			}
-			proc.destroy();
-
-			if (!anythingRendered)
-				throw new NothingRenderedException("Nothing rendered!");
-		} catch (IOException e) {
-			System.out.println(e.getMessage());
-			//TODO: Handle Exception
 		}
+		String fullBlenderFileName = tempDir + blenderFileName;
+		System.out.println("Filename = " + fullBlenderFileName);
+		File file = FileUtil.saveFile(task.getBlenderFile(), fullBlenderFileName);
+		System.out.println("Saved file: " + file);
+		String[] cmdarr = { "blender", "-b", fullBlenderFileName, "-o",
+				tempDir, "-F", task.getFileFormat(), "-s",
+				String.valueOf(task.getStartFrame()), "-e",
+				String.valueOf(task.getEndFrame()), "-a", "-x", "1" };
+		System.out.println("Arguments: ");
+		for(int i = 0; i < cmdarr.length; i++ ) System.out.println(cmdarr[i]);
+		Process proc = Runtime.getRuntime().exec(cmdarr);
+		BufferedReader br = new BufferedReader(new InputStreamReader(proc
+				.getInputStream()));
+
+		String line;
+		System.out.println("Buffered reader: ");
+		while ((line = br.readLine()) != null && !bStopFlag) {
+			System.out.println(line);
+			if (line.startsWith("'blender' is not recognized")
+					|| line.indexOf("blender") != -1
+					&& line.indexOf("not found") != -1)
+			{
+				System.out.println("Blender not found!");
+				break;
+			}
+			else if (line.startsWith("Append frame")
+					|| line.startsWith("added frame")
+					|| line.startsWith("Writing frame"))
+			{
+				System.out.println("Rendered frame " + line.split(" +")[2]);
+			}
+			else if (line.startsWith("Blender quit"))
+			{
+				System.out.println("Rendering finished");
+			}
+		}
+		proc.destroy();
 	}
 
 }
