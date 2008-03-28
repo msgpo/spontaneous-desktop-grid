906015e536983570ff3788d9e86e55b6bb430d46
mysticel <mysticel@e90d3511-7839-0410-915a-49ea6329ddb5>
2008-03-28 09:22:26 +0000

    removed debug

--- java/MPI/src/ee/ut/f2f/mpi/examples/pi/Pi.java
+++ java/MPI/src/ee/ut/f2f/mpi/examples/pi/Pi.java
@@ -1,7 +1,5 @@
 package ee.ut.f2f.mpi.examples.pi;
 
-import java.util.Date;
-
 import ee.ut.f2f.core.mpi.MPI;
 import ee.ut.f2f.core.mpi.MPITask;
 
@@ -10,16 +8,11 @@ public class Pi extends MPITask {
 	 * Calculates pi on 4 (master + 3 peers) computers, with 2 tasks per peer ( 1 + 3 * 2 ).
 	 */
 	public void runTask() {
-		getMPIDebug().println("******************************************************** " + (new Date()));
-		getMPIDebug().println("Start " + (new Date()));
-		int rank, size, i;
+		getMPIDebug().println("Starting calculating Pi : " + getTaskID());
+		int rank, size;
 		double PI25DT = 3.141592653589793238462643;
 		double h, sum, x;
 		MPI().Init(this, 4, 2);
-
-		getMPIDebug().println("1");
-		double startTime = MPI().Wtime();
-
 		size = MPI().COMM_WORLD().Size();
 		rank = MPI().COMM_WORLD().Rank();
 
@@ -33,30 +26,23 @@ public class Pi extends MPITask {
 			n[0] = 1234567; // number of interval
 		}
 
-		getMPIDebug().println("2 rank=" + rank + " size=" + size);
-		MPI().COMM_WORLD().Bcast(n, 0, 1, MPI.INT, 0);
+		MPI().COMM_WORLD().Bcast(n, 0, 1, MPI.INT, 0);// Send n to every peer
 
-		getMPIDebug().println("3 n[0]=" + n[0]);
 		h = 1.0 / (double) n[0];
 		sum = 0.0;
-		for (i = rank + 1; i <= n[0]; i += size) {
+		for (int i = rank + 1; i <= n[0]; i += size) {
 			x = h * ((double) i - 0.5);
 			sum += (4.0 / (1.0 + x * x));
 		}
 		mypi[0] = h * sum;
 
-		getMPIDebug().println("4 mypi[0]=" + mypi[0] + " h=" + h + " sum=" + sum);
-		MPI().COMM_WORLD().Reduce(mypi, 0, pi, 0, 1, MPI.DOUBLE, MPI.SUM, 0);
+		MPI().COMM_WORLD().Reduce(mypi, 0, pi, 0, 1, MPI.DOUBLE, MPI.SUM, 0);// Collect results
 
-		getMPIDebug().println("5 pi[0]=" + pi[0]);
+		getMPIDebug().println("My part of Pi was " + mypi);
 		if (rank == 0) {
 			getMPIDebug().println("Pi is approximately " + pi[0]);
 			getMPIDebug().println("Error is " + (pi[0] - PI25DT));
-			double stopTime = MPI().Wtime();
-			getMPIDebug().println("Time usage = " + (stopTime - startTime) + " ms");
 		}
-
-		getMPIDebug().println("7");
 		MPI().Finalize();
 	}
 }
