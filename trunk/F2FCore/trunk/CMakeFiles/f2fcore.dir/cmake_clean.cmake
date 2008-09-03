b50b343dd850249dfee185ddc894269867a91087
unorbisrath <unorbisrath@e90d3511-7839-0410-915a-49ea6329ddb5>
2008-09-03 10:18:39 +0000

    Initial import of F2FCore - from kdevelop

---
+++ trunk/F2FCore/trunk/CMakeFiles/f2fcore.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+"CMakeFiles/f2fcore.dir/main.o"
+"f2fcore.pdb"
+"f2fcore"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/f2fcore.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
