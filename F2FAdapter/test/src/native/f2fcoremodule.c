4c5abba5f893ce65f60b86c832b01c3f787636b8
timofei77 <timofei77@e90d3511-7839-0410-915a-49ea6329ddb5>
2008-09-16 16:39:07 +0000

    Add build

---
+++ F2FAdapter/test/src/native/f2fcoremodule.c
@@ -0,0 +1,11 @@
+/****************************************************************
+ * A python extension module for F2FCore
+ *
+ ****************************************************************/
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "Python.h"
+#include "f2fcore.h"
+
