dfe5b6cbc38da699a044830edc17d33b3682ca9c
timofei77 <timofei77@e90d3511-7839-0410-915a-49ea6329ddb5>
2008-09-16 18:17:01 +0000

    Simple python extension example in f2fcoremodule.c

--- F2FAdapter/test/src/native/f2fcoremodule.c
+++ F2FAdapter/test/src/native/f2fcoremodule.c
@@ -6,6 +6,35 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "Python.h"
-#include "f2fcore.h"
+#include "python2.5/Python.h"
 
+/* simple example of python extension in C */
+/* module functions */
+static PyObject*
+message(PyObject *self, PyObject *args){
+	char *fromPython;
+	char result[64];
+	int n = -1;
+
+	if (! Py_Arg_Parse(args, "(s)", &fromPython) ){
+		return NULL;
+	} else {
+		strcpy(result, "Hello, ");
+		strcat(result, fromPython);
+		return Py_BuildValue(result);
+	}
+}
+
+/* registration table */
+static struct PyMemberDef hello_methods[] = {
+		{
+				"message",		// function name
+				message,		// C function pointer
+				1				// always tuple
+		},
+		{ NULL, NULL }			// end of table marker
+};
+
+void initHello(){				// called on first import
+	(void) Py_InitModule4("hello", hello_methods);
+}
