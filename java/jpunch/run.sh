bf7f62c3ce84ea54b98878dd92643e1736b14a05
timofei77 <timofei77@e90d3511-7839-0410-915a-49ea6329ddb5>
2009-01-03 02:07:29 +0000

    Modified bash script

--- java/jpunch/run.sh
+++ java/jpunch/run.sh
@@ -1,14 +1,20 @@
 
-if [ $1 = "-dev" ]; then 
-java -cp build/classes:lib/commons-codec-1.3.jar:lib/jstun-0.6.1.jar \
--Djava.util.logging.config.file=./conf/logging.properties \
--Dnet.ulno.jpunch.PropertiesFile=./conf/jpunch.properties \
-net.ulno.jpunch.test.JPunchTest;
-fi;
-
-if [ $1 = "-rel" ]; then
-java -cp jPunch.jar:lib/commons-codec-1.3.jar:lib/jstun-0.6.1.jar \
--Djava.util.logging.config.file=./conf/logging.properties \
--Dnet.ulno.jpunch.PropertiesFile=./conf/jpunch.properties \
-net.ulno.jpunch.test.JPunchTest;
+if [ $# -ge 1 ]; then
+	if [ $1 = "-dev" ]; then 
+		java -cp build/classes:lib/commons-codec-1.3.jar:lib/jstun-0.6.1.jar \
+		-Djava.util.logging.config.file=./conf/logging.properties \
+		-Dnet.ulno.jpunch.PropertiesFile=./conf/jpunch.properties \
+		net.ulno.jpunch.test.JPunchTest;
+	elif [ $1 = "-help" ]; then
+		echo "Usage :";
+		echo "sh run.sh [options]";
+		echo "Options :";
+		echo "-help print this help message";
+		echo "-dev run in development mode";
+	fi;
+else
+	java -cp jPunch.jar:lib/commons-codec-1.3.jar:lib/jstun-0.6.1.jar \
+	-Djava.util.logging.config.file=./conf/logging.properties \
+	-Dnet.ulno.jpunch.PropertiesFile=./conf/jpunch.properties \
+	net.ulno.jpunch.test.JPunchTest;
 fi;
